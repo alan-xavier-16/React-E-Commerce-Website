9fcfbbb92402da56a0e78356e0ede51112b421b1
Alan Xavier <alan_xvr@hotmail.com>
2019-09-08 21:10:40 -0400

    Add ShopPage and CollectionPreview components - Create ShopPage component that renders a CollectionPreview of items - Create a constants file to store default Collections data that will be the initial state of the Shop component - Create CollectionPreview component that renders Shopdata passed down from ShopPage - Add Route to ShopPage in App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 import HomePage from "./pages/homepage/HomePage.component";
+import Shop from "./pages/shop/Shop.component";
 import "./App.css";
 
 function App() {
   return (
     <Switch>
       <Route exact path="/" component={HomePage} />
+      <Route exact path="/shop" component={Shop} />
     </Switch>
   );
 }
