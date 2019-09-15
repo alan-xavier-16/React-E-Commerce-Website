0eefd6478a3c01c10259d65a2db3cc3769b2840d
Alan Xavier <alan_xvr@hotmail.com>
2019-09-14 20:22:42 -0400

    Add Auth user persistence in App.js - Add a subscrition to Firebase to add or verify a user with Firebase - Stores User data to the Firebase Authentication User's database - Clear user data on app close or signout functionality

--- src/App.js
+++ src/App.js
@@ -1,22 +1,50 @@
-import React from "react";
+import React, { Component } from "react";
 import { Switch, Route } from "react-router-dom";
 import HomePage from "./pages/homepage/HomePage.component";
 import Shop from "./pages/shop/Shop.component";
 import Header from "./components/header/Header.component";
 import SignInAndSignOutPage from "./pages/sign-in-and-sign-up/SignInAndSignOut.component";
+
+import { auth } from "./firebase/firebase.utils";
+
 import "./App.css";
 
-function App() {
-  return (
-    <div>
-      <Header />
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route exact path="/shop" component={Shop} />
-        <Route exact path="/signin" component={SignInAndSignOutPage} />
-      </Switch>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentUser: null
+    };
+  }
+
+  /* HANDLES AUTH CHANGES WITH FIREBASE */
+  /* Creates user subscription to app */
+  unsubscribeFromAuth = null;
+
+  /* On User Sign in or Sign up, gets user data from Firebase and sets the state of the application for this user */
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
+      this.setState({ currentUser: user });
+    });
+  }
+
+  /* Clear subscription */
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
+
+  render() {
+    return (
+      <div>
+        <Header />
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route exact path="/shop" component={Shop} />
+          <Route exact path="/signin" component={SignInAndSignOutPage} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
