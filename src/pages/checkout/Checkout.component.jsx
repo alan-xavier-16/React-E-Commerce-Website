4244acb1ebbbf6bddf2e9297b24604bb26c1c273
Alan Xavier <alan_xvr@hotmail.com>
2019-09-22 19:11:45 -0400

    Add CheckoutItem component - Add CheckoutItem component to display cart items - Import CheckoutItem component in Checkout page

--- src/pages/checkout/Checkout.component.jsx
+++ src/pages/checkout/Checkout.component.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
 
+import CheckoutItem from "../../components/checkout-item/CheckoutItem.component";
+
 import { selectCartItems } from "../../redux/cart/cart.selectors";
 import { selectCartTotal } from "../../redux/cart/cart.selectors";
 
@@ -28,7 +30,9 @@ const Checkout = ({ cartItems, total }) => {
         </div>
       </div>
 
-      {cartItems.map(cartItem => cartItem.name)}
+      {cartItems.map(cartItem => (
+        <CheckoutItem key={cartItem.id} cartItem={cartItem} />
+      ))}
 
       <div className="total">
         <span>TOTAL: ${total}</span>
