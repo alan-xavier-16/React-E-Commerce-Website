f5d43f6b019c583b7c561d90acb7db6ca1fa830b
Alan Xavier <alan_xvr@hotmail.com>
2019-09-21 20:49:02 -0400

    Add reduce function to add cart quantity - Use JavaScript reduce function to add the 'quantity' attribute of cart items - Modify cart icon to render total quantity

--- src/components/cart-icon/CartIcon.component.jsx
+++ src/components/cart-icon/CartIcon.component.jsx
@@ -6,11 +6,11 @@ import { toggleCartHidden } from "../../redux/cart/cart.actions.js";
 import "./CartIcon.styles.scss";
 import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
 
-const CartIcon = ({ toggleCartHidden }) => {
+const CartIcon = ({ toggleCartHidden, itemCount }) => {
   return (
     <div className="cart-icon" onClick={toggleCartHidden}>
       <ShoppingIcon className="shopping-icon" />
-      <span className="item-count">0</span>
+      <span className="item-count">{itemCount}</span>
     </div>
   );
 };
@@ -20,7 +20,15 @@ const mapDispatchToProps = dispatch => ({
   toggleCartHidden: () => dispatch(toggleCartHidden())
 });
 
+/* Adds quantity property from cartItem */
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+  itemCount: cartItems.reduce(
+    (accumulatedQuantity, cartItem) => accumulatedQuantity + cartItem.quantity,
+    0
+  )
+});
+
 export default connect(
-  null,
+  mapStateToProps,
   mapDispatchToProps
 )(CartIcon);
