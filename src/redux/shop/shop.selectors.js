55dd4934fb4f7f0e52c936a4e3127a4a53ce44aa
Alan Xavier <alan_xvr@hotmail.com>
2019-09-28 22:38:22 -0400

    Refactor CollectionsOverview for SHOP_DATA stored as an Object - Add a selector to convert the SHOP_DATA object to an array - Use array in CollectionsOverview

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -7,6 +7,15 @@ export const selectCollections = createSelector(
   shop => shop.collections
 );
 
+/* 
+Function takes the SHOP_DATA object and gets the keys as an array.
+Then it maps over the keys, and returns the array of collections
+*/
+export const selectCollectionsForPreview = createSelector(
+  [selectCollections],
+  collections => Object.keys(collections).map(key => collections[key])
+);
+
 /* Function maps over the collections based on the url parameter (e.g. /hats) and return the matching collection, i.e. hats */
 export const selectCollection = collectionUrlParam =>
   createSelector(
