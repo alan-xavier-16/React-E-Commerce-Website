a495a3c955e2064839971f6ac33aa42c5eb827b1
Alan Xavier <alan_xvr@hotmail.com>
2019-09-28 22:25:22 -0400

    Render Collection Items in Collection page - Modify CollectionPage to render the CollectionItem component - Convert 'Shop.data.js' to an object instead of an array for improved optimization and data normalization best-practice - Refactor 'shop.selectors.js' to selectCollection based off of SHOP_DATA object using the url parameter passed in

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -1,14 +1,5 @@
 import { createSelector } from "reselect";
 
-/* String ID and Number ID map for routing to CollectionPage based on the Id of the collection*/
-const COLLECTION_ID_MAP = {
-  hats: 1,
-  sneakers: 2,
-  jackets: 3,
-  womens: 4,
-  mens: 5
-};
-
 const selectShop = state => state.shop;
 
 export const selectCollections = createSelector(
@@ -20,8 +11,5 @@ export const selectCollections = createSelector(
 export const selectCollection = collectionUrlParam =>
   createSelector(
     [selectCollections],
-    collections =>
-      collections.find(
-        collection => collection.id === COLLECTION_ID_MAP[collectionUrlParam]
-      )
+    collections => collections[collectionUrlParam]
   );
